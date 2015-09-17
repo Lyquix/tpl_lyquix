7a275bb878ec296e0c7b29a9bc1ac7fbf2b7a0b4
Ruben Reyes <ruben.reyes@lyquix.com>
2015-09-17 11:54:59 -0400

    Add comment to lyquix.js - the google analytics tracking features only work with the universal code

--- js/lyquix.js
+++ js/lyquix.js
@@ -339,6 +339,17 @@ var lqx = lqx || {
 	initTracking : function() {
 		
 		// check if google analytics have been loaded
+		// NOTE: make sure you are using Google Universal Analytics Code:
+		/*
+		<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-XXXX-Y', 'auto');
+		ga('send', 'pageview');
+		</script>
+		*/
 		if(typeof window.ga !== 'undefined'){
 			
 			// track downloads and outbound links
