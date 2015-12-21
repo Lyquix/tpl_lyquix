51800c596d85915b2877c3e2bf12fa5dba1c7dea
Lyquix <info@lyquix.com>
2015-12-21 17:00:23 -0500

    Update link to browserhappy.com, update template.less

--- index-custom.dist.php
+++ index-custom.dist.php
@@ -398,7 +398,7 @@ if($this->params->get('mobiledetect_method', 'php') == 'php'){
 
 <!--[if lte IE 8]>
 <link href="<?php echo $tmpl_url; ?>/css/ie8-alert.css" rel="stylesheet" />
-<div class="ie8-alert">You are using an unsupported version of Internet Explorer. To ensure security, performance, and full functionality, <a href="http://browsehappy.com/" target="_blank">please upgrade to an up-to-date browser.</a></div>
+<div class="ie8-alert">You are using an unsupported version of Internet Explorer. To ensure security, performance, and full functionality, <a href="http://browsehappy.com/?locale=<?php echo $this->language; ?>" target="_blank">please upgrade to an up-to-date browser.</a></div>
 <![endif]-->
 
 <?php echo $this->params->get('disqus_shortname') ? '<script src="//' . $this->params->get('disqus_shortname') . '.disqus.com/embed.js"></script>' : ''; ?>
