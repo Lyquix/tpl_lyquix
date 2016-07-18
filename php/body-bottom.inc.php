e40a9602484bae911aaa637457935287ea799d7d
Ruben Reyes <ruben.reyes@lyquix.com>
2016-07-18 12:24:30 -0400

    Add option for displaying outdated browser alert in IE9

--- php/body-bottom.inc.php
+++ php/body-bottom.inc.php
@@ -1,8 +1,14 @@
 <!--[if lte IE 8]>
 <link href="<?php echo $tmpl_url; ?>/css/ie8-alert.css" rel="stylesheet" />
-<div class="ie8-alert">You are using an unsupported version of Internet Explorer. To ensure security, performance, and full functionality, <a href="http://browsehappy.com/?locale=<?php echo $this->language; ?>" target="_blank">please upgrade to an up-to-date browser.</a></div>
+<div class="ie8-alert">You are using an unsupported version of Internet Explorer. To ensure security, performance, and full functionality, <a href="http://browsehappy.com/?locale=<?php echo $this->language; ?>">please upgrade to an up-to-date browser.</a></div>
 <![endif]-->
-
-<?php echo $this->params->get('disqus_shortname') ? '<script src="//' . $this->params->get('disqus_shortname') . '.disqus.com/embed.js"></script>' : ''; ?>
+<?php if($this->params->get('ie9_alert',0)): ?>
+<!--[if IE 9]>
+<link href="<?php echo $tmpl_url; ?>/css/ie9-alert.css" rel="stylesheet" />
+<div class="ie9-alert">You are using an unsupported version of Internet Explorer. To ensure security, performance, and full functionality, <a href="http://browsehappy.com/?locale=<?php echo $this->language; ?>">please upgrade to an up-to-date browser.</a><i></i></div>
+<script>jQuery('.ie9-alert i').click(function(){jQuery('.ie9-alert').hide();});</script>
+<![endif]-->
+<?php endif;
+echo $this->params->get('disqus_shortname') ? '<script src="//' . $this->params->get('disqus_shortname') . '.disqus.com/embed.js"></script>' : ''; ?>
 
 <jdoc:include type="modules" name="body" />
