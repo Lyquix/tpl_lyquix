25e1b75d8cf20a1bedaeb2b71aa1c59f4bcebab9
Ruben Reyes <ruben.reyes@lyquix.com>
2016-08-25 16:16:52 -0400

    Add: new fields for controlling Joomla default libraries, and IE8 alert Change: reorganize fields into tabs

--- php/head-pre.inc.php
+++ php/head-pre.inc.php
@@ -1,7 +1,4 @@
 <?php
-// Enable Joomla Bootstrap and jQuery UI framework;
-JHtml::_('bootstrap.framework');
-JHtml::_('jquery.ui', array('core', 'sortable'));
 $doc = JFactory::getDocument();
 
 // Prevent adding component css and js libraries in <jdoc:include type="head" />
@@ -9,6 +6,43 @@ $remove_css_libraries = explode("\n", trim($this->params->get('remove_css_librar
 foreach($remove_css_libraries as $css_url) unset($doc->_styleSheets[trim($css_url)]);
 $remove_js_libraries = explode("\n", trim($this->params->get('remove_js_libraries', '')));
 foreach($remove_js_libraries as $js_url) unset($doc->_scripts[trim($js_url)]);
+
+// Disable MooTools
+if($this->params->get('disable_mootools', 1)) {
+	// css
+	foreach([
+		'/media/system/css/modal.css',
+		] as $css_url) unset($doc->_styleSheets[$site_rel_url . trim($css_url)]);
+	// js
+	foreach([
+		'/media/system/js/mootools-core.js',
+		'/media/system/js/mootools-more.js',
+		'/media/system/js/mootree.js',
+		'/media/system/js/mootools-core-uncompressed.js',
+		'/media/system/js/mootools-more-uncompressed.js',
+		'/media/system/js/mootree-uncompressed.js',
+		] as $js_url) unset($doc->_scripts[$site_rel_url . trim($js_url)]);
+}
+
+// Enable jQuery
+if($this->params->get('enable_jquery', 1)) {
+	JHtml::_('jquery.framework');
+}
+
+// Enable jQuery UI
+if($this->params->get('enable_jquery_ui')) {
+	JHtml::_('jquery.ui', explode(",", $this->params->get('enable_jquery_ui')));
+}
+
+// Enable Bootstrap
+if($this->params->get('enable_bootstrap', 1)) {
+	JHtml::_('bootstrap.framework');
+}
+
+// Joomla Javascript Framework
+if(is_array($this->params->get('joomla_js'))) {
+	foreach($this->params->get('joomla_js') as $joomla_js) JHtml::_('behavior.' . $joomla_js);
+}
 
 // declare some variables
 $home = $mobile = $phone = $tablet = false;
