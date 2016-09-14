26a6df68274cf064e0611e4622dd7c948a951012
Ruben Reyes <ruben.reyes@lyquix.com>
2016-09-14 08:59:06 -0400

    Fix syntax of meta tag (add closing slash)

--- php/head-top.inc.php
+++ php/head-top.inc.php
@@ -15,4 +15,4 @@ if($home) {
 } 
 ?>
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
