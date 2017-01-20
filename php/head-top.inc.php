0305c5f954b13c87a4444a66c4caf195f37cd1ea
Lyquix <info@lyquix.com>
2017-01-20 06:51:29 -0500

    Move X-UA-Compatible meta tag to top of head tag
    The X-UA-Compatible meta tag must be the first meta tag, otherwise, it may not be effective to override Intranet settings
    http://stackoverflow.com/questions/6156639/x-ua-compatible-is-set-to-ie-edge-but-it-still-doesnt-stop-compatibility-mode

--- php/head-top.inc.php
+++ php/head-top.inc.php
@@ -5,6 +5,8 @@
 <script src="<?php echo $tmpl_url; ?>/js/aight<?php echo $this->params->get('non_min_js') ? '' : '.min'; ?>.js"></script>
 <script src="<?php echo $tmpl_url; ?>/js/selectivizr<?php echo $this->params->get('non_min_js') ? '' : '.min'; ?>.js"></script>
 <![endif]-->
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <?php 
 // Adds search engine domain validation strings to home page only
 if($home) {
@@ -13,10 +15,6 @@ if($home) {
 	echo $this->params->get('p_domain_verify') ? '<meta name="p:domain_verify" content="' . $this->params->get('p_domain_verify') . '"/>' . "\n" : '';
 	echo '<link href="' . JURI::root() . '" rel="canonical" />' . "\n";
 } 
-?>
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<?php
 // Add og:title and og:description tags if not already set
 if(!$doc -> getMetaData('og:title')) $doc->addCustomTag('<meta property="og:title" content="' . $doc -> getTitle() . '" />');
 if(!$doc -> getMetaData('og:description')) $doc->addCustomTag('<meta property="og:description" content="' . $doc -> getDescription() . '" />');
