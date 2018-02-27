e9a2a83f33a56f4ac7ed37af6a5b83f0f048c0dc
Ruben Reyes <ruben.reyes@lyquix.com>
2018-02-27 17:08:38 -0500

    IE alert updates:
    Remove alert for IE8
    Alert for IE9 will not use conditional tags
    Single code for alert on IE9, 10 and 11

--- inc/body-bottom.php
+++ inc/body-bottom.php
@@ -10,18 +10,16 @@
  * @link        https://github.com/Lyquix/tpl_lyquix
  */
 
-if($this->params->get('ie8_alert',1)): ?>
-<!--[if lte IE 8]>
-<link href="<?php echo $tmpl_url; ?>/css/ie8-alert.css" rel="stylesheet" />
-<div class="ie8-alert">You are using an unsupported version of Internet Explorer. To ensure security, performance, and full functionality, <a href="http://browsehappy.com/?locale=<?php echo $this->language; ?>">please upgrade to an up-to-date browser.</a></div>
-<![endif]-->
+$ie9 = $this->params->get('ie9_alert',1);
+$ie10 = $this->params->get('ie10_alert',1);
+$ie11 = $this->params->get('ie11_alert',0);
+
+if($ie9 || $ie10 || $ie11) : ?>
+<link href="<?php echo $tmpl_url; ?>/css/ie-alert.css" rel="stylesheet" />
+<div class="ie-alert<?php echo ($ie9 ? ' ie9' : '') . ($ie10 ? ' ie10' : '') . ($ie11 ? ' ie11' : ''); ?>">You are using an unsupported version of Internet Explorer. To ensure security, performance, and full functionality, <a href="http://browsehappy.com/?locale=<?php echo $this->language; ?>">please upgrade to an up-to-date browser.</a><i></i></div>
+<script>
+jQuery('.ie-alert i').click(function(){jQuery('.ie-alert').hide();});
+</script>
 <?php endif;
-if($this->params->get('ie9_alert',1)): ?>
-<!--[if IE 9]>
-<link href="<?php echo $tmpl_url; ?>/css/ie9-alert.css" rel="stylesheet" />
-<div class="ie9-alert">You are using an unsupported version of Internet Explorer. To ensure security, performance, and full functionality, <a href="http://browsehappy.com/?locale=<?php echo $this->language; ?>">please upgrade to an up-to-date browser.</a><i></i></div>
-<script>jQuery('.ie9-alert i').click(function(){jQuery('.ie9-alert').hide();});</script>
-<![endif]-->
-<?php endif; ?>
 
 <jdoc:include type="modules" name="body" />
