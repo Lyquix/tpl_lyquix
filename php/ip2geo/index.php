cc22ace759fe5f346d57ef1eeae0f33c89b913e7
Ruben Reyes <ruben.reyes@lyquix.com>
2018-07-18 13:03:24 -0400

    Fixes and updates for ip2geo library

--- php/ip2geo/index.php
+++ php/ip2geo/index.php
@@ -12,41 +12,44 @@
 
 header('Content-Type: application/json');
 $db = 'GeoLite2-City.mmdb'; // download updated database from http://dev.maxmind.com/geoip/geoip2/geolite2/
-require_once 'Reader.php';
-require_once 'Decoder.php';
-require_once 'InvalidDatabaseException.php';
-require_once 'Metadata.php';
-require_once 'Util.php';
 
-use MaxMind\Db\Reader;
+if(file_exists($db)) {
+	require_once 'Reader.php';
+	require_once 'Decoder.php';
+	require_once 'InvalidDatabaseException.php';
+	require_once 'Metadata.php';
+	require_once 'Util.php';
 
-// Get IP address from HTTP request
-$ip = $_SERVER['REMOTE_ADDR'];
+	// Get IP address from HTTP request
+	$ip = $_SERVER['REMOTE_ADDR'];
 
-// Sanitize IP address
-$ip = filter_var($ip, FILTER_VALIDATE_IP);
+	// Sanitize IP address
+	$ip = filter_var($ip, FILTER_VALIDATE_IP);
 
-if($ip) {
-	$reader = new Reader($db);
-	$geo = $reader->get($ip);
-	$reader->close();
-	$geo = Array(
-		'city' => $geo['city']['names']['en'],
-		'subdivision' => $geo['subdivisions'][0]['names']['en'],
-		'country' => $geo['country']['iso_code'], 
-		'continent' => $geo['continent']['code'],
-		'time_zone' => $geo['location']['time_zone'],
-		'lat' => $geo['location']['latitude'],
-		'lon' => $geo['location']['longitude'],
-		'radius' => $geo['location']['accuracy_radius'] // in km
-	);
-	echo json_encode($geo);
+	if($ip) {
+		$reader = new Reader($db);
+		$geo = $reader->get($ip);
+		$reader->close();
+		$geo = Array(
+			'city' => $geo['city']['names']['en'],
+			'subdivision' => $geo['subdivisions'][0]['names']['en'],
+			'country' => $geo['country']['iso_code'], 
+			'continent' => $geo['continent']['code'],
+			'time_zone' => $geo['location']['time_zone'],
+			'lat' => $geo['location']['latitude'],
+			'lon' => $geo['location']['longitude'],
+			'radius' => $geo['location']['accuracy_radius'] // in km
+		);
+		echo json_encode($geo);
+	}
+	else {
+		echo '{}';
+	}
 }
 else {
 	echo '{}';
 }
 exit;
-
 /*
 
 References
@@ -63,4 +66,3 @@ References
 > Time Zones: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 
 */
-
