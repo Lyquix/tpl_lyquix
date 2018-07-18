4a2782975036ef8cd62b0cb2a32c1cbc7aa5e986
Ruben Reyes <ruben.reyes@lyquix.com>
2018-07-18 13:03:44 -0400

    Bash script to compile CSS files

---
+++ css/css.sh
@@ -0,0 +1,20 @@
+###
+#
+# css.sh - Bash script to process SCSS files, run autoprefixer, and chunk files 
+#
+# @version     2.0.0
+# @package     tpl_lyquix
+# @author      Lyquix
+# @copyright   Copyright (C) 2015 - 2018 Lyquix
+# @license     GNU General Public License version 2 or later
+# @link        https://github.com/Lyquix/tpl_lyquix
+#
+###
+
+# Get script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+sass $DIR/styles.scss > $DIR/styles.css
+postcss -u autoprefixer --autoprefixer.browsers "> 0.5%, last 3 versions" -r $DIR/styles.css
+blessc chunk $DIR/styles.css
+sass $DIR/template.scss > $DIR/template.css
