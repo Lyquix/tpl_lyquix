94ab092a1cdf72d79c8ea63062a77cfd57fae131
Ruben Reyes <ruben.reyes@lyquix.com>
2018-08-02 11:28:23 -0400

    Add sample Vue controller and component

---
+++ js/custom/components/component.dist.js
@@ -0,0 +1,14 @@
+/**
+ * component.dist.js - Sample Vue component
+ *
+ * @version     2.0.0
+ * @package     tpl_lyquix
+ * @author      Lyquix
+ * @copyright   Copyright (C) 2015 - 2018 Lyquix
+ * @license     GNU General Public License version 2 or later
+ * @link        https://github.com/Lyquix/tpl_lyquix
+ */
+
+Vue.component('my-component', {
+	template: '<h1>{{ message }}</h1>'
+})
---
+++ js/custom/controllers/controller.dist.js
@@ -0,0 +1,21 @@
+/**
+ * controller.dist.js - Sample Vue controller
+ *
+ * @version     2.0.0
+ * @package     tpl_lyquix
+ * @author      Lyquix
+ * @copyright   Copyright (C) 2015 - 2018 Lyquix
+ * @license     GNU General Public License version 2 or later
+ * @link        https://github.com/Lyquix/tpl_lyquix
+ */
+
+document.addEventListener('DOMContentLoaded', function(){
+	if(document.querySelector('#my-controller')) {
+		var myapp = new Vue({
+			el: '#my-controller',
+			data: {
+				message: 'Hello World!'
+			}
+		});
+	}
+});
