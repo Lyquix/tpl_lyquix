728afdf24a7d7de29c32b44d1b1c9eb1bdc72848
Ruben Reyes <ruben.reyes@lyquix.com>
2018-08-18 15:58:14 -0400

    Add polyfill.io

--- php/meta.php
+++ php/meta.php
@@ -16,6 +16,10 @@ $doc -> setGenerator('');
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <?php
+// Load polyfill.io
+if($this->params->get('polyfill', 1)): ?>
+<script async src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>
+<?php endif;
 // Adds search engine domain validation strings to home page only
 if($home) {
 	echo $this -> params -> get('google_site_verification') ? '<meta name="google-site-verification" content="' . htmlentities($this -> params -> get('google_site_verification')) . '" />' . "\n" : '';
