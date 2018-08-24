15ea8b42de1d7c420fb02b217d6c9c6213b5c72c
Ruben Reyes <ruben.reyes@lyquix.com>
2018-08-23 21:01:48 -0400

    Add new string module to js.dist.sh

--- js/js.dist.sh
+++ js/js.dist.sh
@@ -15,7 +15,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
 # Lyquix
 cat $DIR/lib/core.js > $DIR/lyquix.js
-MODULES=("util" "detect" "geolocate" "mutation" "responsive" "fixes" "menu" "accordion" "autoresize" "tabs" "lyqbox" "analytics")
+MODULES=("util" "detect" "geolocate" "mutation" "responsive" "fixes" "menu" "accordion" "autoresize" "tabs" "string" "lyqbox" "analytics")
 for MOD in "${MODULES[@]}"
 do
 	cat $DIR/lib/$MOD.js >> $DIR/lyquix.js
