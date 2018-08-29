87d17f0b69e1f07cb8e46fb18db0f5d2e76af00e
Ruben Reyes <ruben.reyes@lyquix.com>
2018-08-29 15:56:55 -0400

    Add: gulpfile.js to automatically process CSS and JS files

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+/**
+ * gulpfile.js - Watch and automatically process CSS and JS
+ *
+ * @version     2.0.0-beta-3
+ * @package     tpl_lyquix
+ * @author      Lyquix
+ * @copyright   Copyright (C) 2015 - 2018 Lyquix
+ * @license     GNU General Public License version 2 or later
+ * @link        https://github.com/Lyquix/tpl_lyquix
+ */
+
+var gulp = require('gulp'),
+	shell = require('gulp-shell');
+
+function errorLog (error) {
+	console.error.bind(error);
+	this.emit('end');
+}
+
+gulp.task('process-css', function(){
+	return gulp.src(['css/styles.scss'])
+		.on('error', errorLog)
+		.pipe(shell(['bash ' + process.cwd() + '/css/css.sh']));
+});
+
+gulp.task('process-js', function(){
+	return gulp.src(['js/scripts.js'])
+		.on('error', errorLog)
+		.pipe(shell(['bash ' + process.cwd() + '/js/js.sh']));
+});
+
+gulp.task('default', function () {
+	gulp.watch(['./css/styles.scss', './css/custom/*.scss'], ['process-css']);
+	gulp.watch(['./js/scripts.js', './js/custom/*.js', './js/custom/components/*.js', './js/custom/controllers/*.js'], ['process-js']);
+});
