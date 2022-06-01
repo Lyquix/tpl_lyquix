41cd43b456bb41ba8825096ebf94160d991f367a
maustin92 <matt.austin@lyquix.com>
2022-06-01 09:17:54 -0400

    Change: move gtm to an earlier position to prevent timing errors

--- php/head-bottom.inc.php
+++ php/head-bottom.inc.php
@@ -9,13 +9,5 @@
  * @license     GNU General Public License version 2 or later
  * @link        https://github.com/Lyquix/tpl_lyquix
  */
-
-echo $this->params->get('gtm_account') ? "<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','" . $this->params->get('gtm_account') . "');</script>
-<!-- End Google Tag Manager -->" : '';
 ?>
 <jdoc:include type="modules" name="head" />
