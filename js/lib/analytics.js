0c0e80c818b8834d925bac2a39047fd174066aec
paulllee <paulllee.md@gmail.com>
2022-08-02 14:28:38 -0400

    Update old code to Google Analytics 4 code

--- js/lib/analytics.js
+++ js/lib/analytics.js
@@ -140,18 +140,11 @@ if(lqx && !('analytics' in lqx)) {
 
 		var gaCode = function() {
 			lqx.log('Loading Google Analytics code');
-			(function (i, s, o, g, r, a, m) {
-				i.GoogleAnalyticsObject = r;
-				i[r] = i[r] || function() {
-					(i[r].q = i[r].q || []).push(arguments);
-				};
-				i[r].l = 1 * new Date();
-				a = s.createElement(o);
-				m = s.getElementsByTagName(o)[0];
-				a.async = 1;
-				a.src = g;
-				m.parentNode.insertBefore(a, m);
-			})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+		  
+			gtag('config', params[tracker].trackingId);
 
 			// Create commands
 			var params = opts.createParams;
