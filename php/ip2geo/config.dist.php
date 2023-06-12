605f4a3f90a679fe7378fa54d27999b381025646
rubenreyes2000 <ruben.reyes@lyquix.com>
2023-06-12 16:40:39 -0400

    ip2geo improvements

---
+++ php/ip2geo/config.dist.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * config.dist.php - Configuration settings for ip2geo functionality
+ *
+ * @version     1.0.12
+ * @package     tpl_lyquix
+ * @author      Lyquix
+ * @copyright   Copyright (C) 2015 - 2018 Lyquix
+ * @license     GNU General Public License version 2 or later
+ * @link        https://github.com/Lyquix/tpl_lyquix
+ */
+
+$config = [
+	'maxmind_license_key' => '',
+	'max_db_age' => 90, // Maximum time allowed until updated database is downloaded, in days (default: 90)
+];
